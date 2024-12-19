ba2b31b3f54b56501155cfb8df9a49be8c33617b
faima-ahmed <faimaahmeddiya@gmail.com>
2024-12-20 03:33:55 +0600

    Commit 1

--- src/features/products/ProductListView.jsx
+++ src/features/products/ProductListView.jsx
@@ -1,15 +1,29 @@
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchProducts } from "./productSlice";
 
 const ProductListView = () => {
+  const {products, isLoading, error}= useSelector((state)=>
+    state.productsR
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchProducts());
-  }, []);
+  }, [dispatch]);
 
-  return <div>List of products</div>;
+  return (
+    <div>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {!isLoading && !error && products && products.length>0 && 
+      products.map((product)=>{
+        return <article key={product.id}>
+          <h3>{product.title}</h3>
+        </article>
+      })}
+    </div>
+  );
 };
 
 export default ProductListView;
