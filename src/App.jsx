2b215d19e590caf7ebb7bd428ff12cbb867fafdf
faima-ahmed <faimaahmeddiya@gmail.com>
2025-01-05 15:39:48 +0600

    asyncThunk

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import ProductListView from './features/products/ProductListView';
 
 const App = () => {
--- src/features/products/ProductListView.jsx
+++ src/features/products/ProductListView.jsx
@@ -1,4 +1,16 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 const productListView = () => {
+
+    const fetchProducts = async()=>{
+        const res= await axios.get('http://localhost:3003/products');
+        console.log(res.data);
+      }
+       useEffect(()=>{
+      fetchProducts();
+        }, []);
+
     return (
         <div>
             List of products
